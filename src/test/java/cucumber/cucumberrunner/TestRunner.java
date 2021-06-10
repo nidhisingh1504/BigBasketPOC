7bbd64c0ad5cca72b66cac120e9c987fa3d3aa32
nidhisingh1504 <85605050+nidhisingh1504@users.noreply.github.com>
2021-06-10 16:58:19 +0530

    Update Runner
    Update Runner

--- src/test/java/cucumber/cucumberrunner/TestRunner.java
+++ src/test/java/cucumber/cucumberrunner/TestRunner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features ="src/test/java/cucumber/features/features/BigBasket.feature",
+		features ="src/test/java/cucumber/features/BigBasket.feature",
 		glue="stepDefinations")
 public class TestRunner {
 
